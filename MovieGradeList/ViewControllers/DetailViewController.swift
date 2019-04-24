c7acbffa64f21437d5c3afda68b4303ebc2c5d7b
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2019-04-24 20:31:32 +0900

    DetailViewController 생성

---
+++ MovieGradeList/ViewControllers/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  MovieGradeList
+//
+//  Created by 이동건 on 24/04/2019.
+//  Copyright © 2019 이동건. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
