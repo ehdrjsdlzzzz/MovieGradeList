ca82bced39cf87c96c560f9ec4ea3f45d631fbb8
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2019-04-24 20:36:58 +0900

    StarRatingView 생성

---
+++ MovieGradeList/Views/StarRatingView.swift
@@ -0,0 +1,21 @@
+//
+//  StarRatingView.swift
+//  MovieGradeList
+//
+//  Created by 이동건 on 24/04/2019.
+//  Copyright © 2019 이동건. All rights reserved.
+//
+
+import UIKit
+
+class StarRatingView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
