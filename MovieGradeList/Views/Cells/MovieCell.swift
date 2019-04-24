8866ea8109f7d0835e37fd5bc853f4f7e9fbc30f
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2019-04-24 18:46:00 +0900

    영화 셀 기본 구성

---
+++ MovieGradeList/Views/Cells/MovieCell.swift
@@ -0,0 +1,21 @@
+//
+//  MovieCell.swift
+//  MovieGradeList
+//
+//  Created by 이동건 on 24/04/2019.
+//  Copyright © 2019 이동건. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UICollectionViewCell {
+
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+}
