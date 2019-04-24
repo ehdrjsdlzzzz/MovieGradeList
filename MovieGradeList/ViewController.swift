2ae6c01c906fd924f735af4c145f5ea70194b308
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2019-04-24 17:38:40 +0900

    first commit

---
+++ MovieGradeList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieGradeList
+//
+//  Created by 이동건 on 24/04/2019.
+//  Copyright © 2019 이동건. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
