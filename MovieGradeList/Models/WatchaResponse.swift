c12fe0da096b47cbdda6a16ed2f41d12cdd04b49
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2019-04-25 23:54:45 +0900

    상세 뷰 모델 정의 및 평점 프로퍼티

--- MovieGradeList/Models/WatchaResponse.swift
+++ MovieGradeList/Models/WatchaResponse.swift
@@ -20,10 +20,20 @@ struct Movie: Decodable {
     private var stillcut: String
     private var nation: String
     private var genre: String
+    private var rating: Double? = 0
     
-    var listModel: List? {
+    var listType: List? {
         let description = "\(year) • \(nation)"
-        return List(title: title, posterURL: URL(string: poster), description: description)
+        return List(title: title, posterURL: URL(string: poster), description: description, rating: rating ?? 0)
+    }
+    
+    var detailType: Detail? {
+        let description = "\(year) • \(nation) • \(genre)"
+        return Detail(title: title, posterURL: URL(string: poster), stillcutURL: URL(string: stillcut), description: description, rating: rating)
+    }
+    
+    mutating func updateRating(_ rating: Double) {
+        self.rating = rating
     }
 }
 
@@ -32,5 +42,14 @@ extension Movie {
         var title: String
         var posterURL: URL?
         var description: String
+        var rating: Double
+    }
+    
+    struct Detail {
+        var title: String
+        var posterURL: URL?
+        var stillcutURL: URL?
+        var description: String
+        var rating: Double?
     }
 }
