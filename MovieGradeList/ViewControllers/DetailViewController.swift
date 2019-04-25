b9d913837dee24cfdae96bdf7a40166d271d2000
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2019-04-25 23:56:29 +0900

    영화 상세 화면 구성 및 StarRatingView 프로토콜 구현

--- MovieGradeList/ViewControllers/DetailViewController.swift
+++ MovieGradeList/ViewControllers/DetailViewController.swift
@@ -8,23 +8,56 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+protocol MovieDetailViewDataSource: class {
+    func movieDetail() -> Movie.Detail?
+}
 
+protocol MovieDetailViewDelegate: class {
+    func movieDetailView(_ movieDetailView: DetailViewController, ratingDidChanged: Double)
+}
+
+class DetailViewController: UIViewController {
+    weak var dataSource: MovieDetailViewDataSource?
+    weak var delegate: MovieDetailViewDelegate?
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var stillcutImageView: UIImageView!
+    @IBOutlet weak var ratingInfoLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var starRatingView: StarRatingView!
+    
+    private var movie: Movie.Detail?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        starRatingView.dataSource = self
+        starRatingView.delegate = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configure()
     }
-    */
+    
+    private func configure() {
+        movie = dataSource?.movieDetail()
+        titleLabel.text = movie?.title
+        descriptionLabel.text = movie?.description
+        starRatingView.enableTapGesture()
+        starRatingView.enablePanGesture()
+        starRatingView.layoutIfNeeded()
+    }
+}
 
+extension DetailViewController: StarRatingViewDelegate {
+    func starRatingView(_ starRatingView: StarRatingView, ratingDidChanged: Double) {
+        delegate?.movieDetailView(self, ratingDidChanged: ratingDidChanged)
+    }
+}
+
+extension DetailViewController: StarRatingViewDataSource {
+    func ratings() -> Double? {
+        return movie?.rating
+    }
 }
