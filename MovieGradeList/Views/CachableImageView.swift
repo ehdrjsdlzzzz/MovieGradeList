5a45748c13c90db082f184658668247807cd02b0
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2019-04-26 00:43:27 +0900

    이미지 캐싱을 위한 CachableImageView 생성

---
+++ MovieGradeList/Views/CachableImageView.swift
@@ -0,0 +1,42 @@
+//
+//  CachableImageView.swift
+//  MovieGradeList
+//
+//  Created by 이동건 on 26/04/2019.
+//  Copyright © 2019 이동건. All rights reserved.
+//
+
+import UIKit
+
+class CachableImageView: UIImageView {
+    private var lastURLUsedToLoadImage: String?
+    
+    func loadImage(urlString: String) {
+        lastURLUsedToLoadImage = urlString
+        
+        if let cachedImage = ImageCacheService.default.image(key: urlString) {
+            self.image = cachedImage
+            return
+        }
+        
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            if let err = err {
+                print("Failed to fetch post image:", err)
+                return
+            }
+            
+            if url.absoluteString != self.lastURLUsedToLoadImage {
+                return
+            }
+            
+            guard let imageData = data else { return }
+            guard let image = UIImage(data: imageData) else { return }
+            ImageCacheService.default.save(key: url.absoluteString, value: image)
+            DispatchQueue.main.async {
+                self.image = image
+            }
+            }.resume()
+    }
+}
